97af9a745416c3ca3a8b01ecbc039d4d9fc7431d
Георгий Чудаков <george1005@yandex.ru>
2022-04-27 10:11:16 +0300

    chain+worker

---
+++ Assets/Code/Controllers/TimeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Controllers
+{
+    public sealed class TimeController: IOnController, IOnUpdate
+    {
+        private float _endTime;
+        private bool _isTimerOn;
+        private bool _timerIsDone;
+        //TODO: Посмотреть у Романа
+        //TODO: https://github.com/RomanAstra/Patterns
+        public void OnUpdate(float deltaTime)
+        {
+            if (_isTimerOn)
+                    if (_endTime <= Time.time)
+                    {
+                        _isTimerOn = false;
+                        _timerIsDone = true;
+                    }
+        }
+
+        public void AddTimer(float secondsToTheEnd)
+        {
+            _isTimerOn = false;
+            SetEndTime(secondsToTheEnd);
+        }
+        
+        private void SetEndTime(float secondsToTheEnd)
+        {
+            _endTime = secondsToTheEnd+Time.time;
+        }
+
+        public void TurnOnTimer()
+        {
+            _isTimerOn = true;
+            _timerIsDone = false;
+        }
+
+        public bool TimerIsDone()
+        {
+            return _timerIsDone;
+        }
+    }
+}
