f7f36c7d111f940f696e1110f3470593edce15c3
MalSan-project <KeysiRayback@gmail.com>
2022-06-09 19:21:13 +0300

    Building system ver 0.000003
    Доделал UI  и его контроллер для производства и строительства.
    - сделал лайтовые правки, немного почистил грязь
    Погружаюсь в пучину "моделирования"...Долгая рутинная и монотонная работа. Потом приступлю к тестингу.
    Если получится - можно пробовать интегрировать уже вместе с миньёнами.

--- Assets/Code/Equipment System/Resurses System/View/BuildingsUI.cs
+++ Assets/Code/Equipment System/Resurses System/View/BuildingsUI.cs
@@ -60,7 +60,46 @@ public class BuildingsUI : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI magikStoneTextholder;
 
+    [SerializeField]
+    private GameObject ProduceUI;
+    [SerializeField]
+    private TextMeshProUGUI ProduceTitle;
+    [SerializeField]
+    public Button StartProduceButton;
+    [SerializeField]
+    public Button AutoProduceButton;
+    [SerializeField]
+    private Slider ProduceLoadSlider;
+    [SerializeField]
+    private Image ProduceIcon;
+    [SerializeField]
+    private TextMeshProUGUI ProduceValue;
 
+    [SerializeField]
+    private GameObject Cost1Holder;
+    [SerializeField]
+    private Image Cost1Icon;
+    [SerializeField]
+    private TextMeshProUGUI Cost1Value;
+
+    [SerializeField]
+    private GameObject Cost2Holder;
+    [SerializeField]
+    private Image Cost2Icon;
+    [SerializeField]
+    private TextMeshProUGUI Cost2Value;
+
+    [SerializeField]
+    private GameObject Cost3Holder;
+    [SerializeField]
+    private Image Cost3Icon;
+    [SerializeField]
+    private TextMeshProUGUI Cost3Value;
+
+    [SerializeField]
+    private GameObject BuildSpace;
+    [SerializeField]
+    private Slider BuildSlider;
     #endregion
     public void SetWoodValue(Sprite icon, int currvalue,int maxvalue)
     {
@@ -121,9 +160,73 @@ public class BuildingsUI : MonoBehaviour
         woodHolder.SetActive(false);
     }
 
+    public void SetValueProduceLoad(float value)
+    {
+        ProduceLoadSlider.value = value;
+    }
+    public void SetProduceInfo(Sprite icon, int produceValue,float produceTime)
+    {
+        ProduceIcon.sprite = icon;
+        ProduceValue.text = $"{produceValue}";
+        ProduceLoadSlider.maxValue = produceTime;
+    }
+    public void SetCostInfo(Sprite icon1, int value1, Sprite icon2, int value2)
+    {
+        Cost1Icon.sprite=icon1;
+        Cost1Value.text = $"{value1}";
+        Cost2Icon.sprite = icon2;
+        Cost2Value.text = $"{value2}";
+        Cost1Holder.SetActive(true);
+        Cost2Holder.SetActive(true);
+        Cost3Holder.SetActive(false);
+    }
+    public void SetCostInfo(Sprite icon1, int value1, Sprite icon2, int value2, Sprite icon3, int value3)
+    {
+        Cost1Icon.sprite = icon1;
+        Cost1Value.text = $"{value1}";
+        Cost2Icon.sprite = icon2;
+        Cost2Value.text = $"{value2}";
+        Cost3Icon.sprite = icon3;
+        Cost3Value.text = $"{value3}";
+        Cost1Holder.SetActive(true);
+        Cost2Holder.SetActive(true);
+        Cost3Holder.SetActive(true);
+    }
+    public void SetCostInfo(Sprite icon1, int value1)
+    {
+        Cost1Icon.sprite = icon1;
+        Cost1Value.text = $"{value1}";
+        Cost1Holder.SetActive(true);
+        Cost2Holder.SetActive(false);
+        Cost3Holder.SetActive(false);
+    }
+    
+    public void DisableProduceUI()
+    {
+        ProduceUI.SetActive(false);
+    }
+    public void SetActiveProduceUI()
+    {
+        ProduceUI.SetActive(true);
+    }
+    public void DisableBuildUI()
+    {
+        BuildSpace.SetActive(false);
+    }
+    public void SetActiveBuildUI(float buildTime)
+    {
+        BuildSpace.SetActive(true);
+        BuildSlider.maxValue = buildTime;
+    }
+    public void SetBuildSliderValue (float value)
+    {
+        BuildSlider.value = value;
+    }
+
     private void Awake()
     {
         gameObject.SetActive(false);
+        DisableProduceUI();
     }
     
 
